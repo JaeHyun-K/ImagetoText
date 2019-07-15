0e9ffb18eb2b2ad0fb3c00b190a342ba07816615
JaeHyun-K <poohjenny710@kaist.ac.kr>
2019-07-15 21:29:20 +0900

    final

--- app/src/main/java/com/example/imagetotext/SplashActivity.java
+++ app/src/main/java/com/example/imagetotext/SplashActivity.java
@@ -11,18 +11,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
     private ImageView iv1;
-//    private ImageView iv2;
-//    private ImageView iv3;
-//    private ImageView iv4;
-//    private ImageView iv5;
+    private ImageView iv2;
+    private ImageView iv3;
+    private ImageView iv4;
+    private ImageView iv5;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         iv1=(ImageView)findViewById(R.id.iv1);
+        iv2=(ImageView)findViewById(R.id.iv2);
+        iv3=(ImageView)findViewById(R.id.iv3);
+        iv4=(ImageView)findViewById(R.id.iv4);
+        iv5=(ImageView)findViewById(R.id.iv5);
         Animation myanim= AnimationUtils.loadAnimation(this,R.anim.mytransition);
         iv1.startAnimation(myanim);
+        iv2.startAnimation(myanim);
+        iv3.startAnimation(myanim);
+        iv4.startAnimation(myanim);
+        iv5.startAnimation(myanim);
 
 
         final Intent i=new Intent(this,MainActivity.class);
